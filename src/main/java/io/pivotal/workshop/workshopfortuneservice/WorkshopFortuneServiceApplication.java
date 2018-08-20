d8eb21b372d248a05ae53040e710b00b592a26bd
David Caron <dcaron@xs4all.nl>
2018-08-20 13:50:21 +0200

    initial

---
+++ src/main/java/io/pivotal/workshop/workshopfortuneservice/WorkshopFortuneServiceApplication.java
@@ -0,0 +1,12 @@
+package io.pivotal.workshop.workshopfortuneservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopFortuneServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopFortuneServiceApplication.class, args);
+	}
+}
