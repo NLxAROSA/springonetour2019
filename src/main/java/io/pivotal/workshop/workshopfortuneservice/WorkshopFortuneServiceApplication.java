39e017c8cefa7e0d92548cc79e0ac31a44b8150e
David Caron <dcaron@xs4all.nl>
2018-08-20 16:36:33 +0200

    added service registry

--- src/main/java/io/pivotal/workshop/workshopfortuneservice/WorkshopFortuneServiceApplication.java
+++ src/main/java/io/pivotal/workshop/workshopfortuneservice/WorkshopFortuneServiceApplication.java
@@ -4,10 +4,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 @Slf4j
 @SpringBootApplication
+@EnableDiscoveryClient
 public class WorkshopFortuneServiceApplication {
 
 	public static void main(String[] args) {
