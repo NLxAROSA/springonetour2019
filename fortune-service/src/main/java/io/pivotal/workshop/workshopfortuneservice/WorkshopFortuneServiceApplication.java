d5c4e020bbf3bd5a87313998f42ff54d15dd37c8
Lars Rosenquist <lrosenquist@pivotal.io>
2019-03-18 16:13:28 +0100

    Removed service registry from fortune service.

--- fortune-service/src/main/java/io/pivotal/workshop/workshopfortuneservice/WorkshopFortuneServiceApplication.java
+++ fortune-service/src/main/java/io/pivotal/workshop/workshopfortuneservice/WorkshopFortuneServiceApplication.java
@@ -5,11 +5,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class WorkshopFortuneServiceApplication {
 
 	private static final Logger log = LoggerFactory.getLogger(WorkshopFortuneServiceApplication.class);
