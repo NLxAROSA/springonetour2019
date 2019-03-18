b4fcf16a03ebc7bde3f5836bdb79db679a89445e
Lars Rosenquist <lrosenquist@pivotal.io>
2019-03-18 16:21:46 +0100

    Added resilience4j circuit breaker.

--- fortune-ui/src/main/java/io/pivotal/workshop/workshopgreetingui/FortuneService.java
+++ fortune-ui/src/main/java/io/pivotal/workshop/workshopgreetingui/FortuneService.java
@@ -1,29 +1,52 @@
 package io.pivotal.workshop.workshopgreetingui;
 
+import java.time.Duration;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import io.github.resilience4j.circuitbreaker.CircuitBreaker;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.vavr.CheckedFunction0;
+import io.vavr.control.Try;
+
 @Component
 public class FortuneService {
 
-  Logger logger = LoggerFactory
-          .getLogger(FortuneService.class);
+  Logger log = LoggerFactory.getLogger(FortuneService.class);
 
   private final RestTemplate restTemplate;
+  private final CircuitBreaker circuitBreaker;
 
   public FortuneService(RestTemplate restTemplate) {
     this.restTemplate = restTemplate;
+    this.circuitBreaker = createCircuitBreaker();
+  }
+
+  private CircuitBreaker createCircuitBreaker() {
+    CircuitBreakerConfig circuitBreakerConfig = CircuitBreakerConfig.custom().failureRateThreshold(50)
+        .waitDurationInOpenState(Duration.ofMillis(20000)).build();
+    CircuitBreaker circuitBreaker = CircuitBreaker.of("resilience-provider", circuitBreakerConfig);
+    circuitBreaker.getEventPublisher().onSuccess(event -> log.info("Call success via circuit breaker"))
+        .onCallNotPermitted(event -> log.info("Call denied by circuit breaker"))
+        .onError(event -> log.info("Call failed via circuit breaker"));
+    return circuitBreaker;
   }
 
   public String getFortune() {
-    String fortune = restTemplate.getForObject("http://workshop-fortune-service.apps.internal:8080/", String.class);
-    return fortune;
+    CheckedFunction0<String> someServiceCall = CircuitBreaker.decorateCheckedSupplier(circuitBreaker,
+        () -> this.remoteFortune());
+    Try<String> result = Try.of(someServiceCall).recover((throwable) -> this.defaultFortune());
+    return result.get();
   }
 
-  public String defaultFortune(){
-    logger.debug("Default fortune used.");
+  private String remoteFortune() {
+    return restTemplate.getForObject("http://workshop-fortune-service.apps.internal:8080/", String.class);
+  }
+
+  private String defaultFortune() {
     return "This fortune is no good. Try another.";
   }
 
