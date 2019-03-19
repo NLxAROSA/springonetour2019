4cbcdb31cd23aa10580322d4c3c86162aa4d184f
Lars Rosenquist <lrosenquist@pivotal.io>
2019-03-19 22:47:02 +0100

    Updated fortunes

--- fortune-service/src/main/java/io/pivotal/workshop/workshopfortuneservice/WorkshopFortuneServiceApplication.java
+++ fortune-service/src/main/java/io/pivotal/workshop/workshopfortuneservice/WorkshopFortuneServiceApplication.java
@@ -20,12 +20,12 @@ public class WorkshopFortuneServiceApplication {
 	CommandLineRunner loadDatabase(FortuneRepository fortuneRepo) {
 		return args -> {
 			log.debug("loading database..");
-			fortuneRepo.save(new Fortune("Do what works."));
-			fortuneRepo.save(new Fortune("Do the right thing."));
-			fortuneRepo.save(new Fortune("Always be kind."));
-			fortuneRepo.save(new Fortune("You learn from your mistakes... You will learn a lot today."));
-			fortuneRepo.save(new Fortune("You can always find happiness at work on Friday."));
-			fortuneRepo.save(new Fortune("You will be hungry again in one hour."));
+			fortuneRepo.save(new Fortune("Do what works!"));
+			fortuneRepo.save(new Fortune("Do the right thing!"));
+			fortuneRepo.save(new Fortune("Always be kind!"));
+			fortuneRepo.save(new Fortune("Today is a good day to dye!"));
+			fortuneRepo.save(new Fortune("There's a 5 in the clock somewhere."));
+			fortuneRepo.save(new Fortune("You will be hungry in an hour."));
 			fortuneRepo.save(new Fortune("Today will be an awesome day!"));
 			log.info("Fortune Repo record count: {}", fortuneRepo.count());
 			fortuneRepo.findAll().forEach(x -> log.debug(x.toString()));
