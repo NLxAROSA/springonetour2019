3e2316382364a0dea307f4e809f1f79bca331484
David Caron <dcaron@xs4all.nl>
2019-03-20 14:25:09 +0100

    Add custom metric

--- fortune-service/src/main/java/io/pivotal/workshop/workshopfortuneservice/FortuneController.java
+++ fortune-service/src/main/java/io/pivotal/workshop/workshopfortuneservice/FortuneController.java
@@ -1,5 +1,6 @@
 package io.pivotal.workshop.workshopfortuneservice;
 
+import io.micrometer.core.instrument.Metrics;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +15,7 @@ public class FortuneController {
 
     @GetMapping
     public String fortune() {
+        Metrics.counter("fortune_teller_fortunes_served").increment(1.0);
         return repository.findRandomFortune().getText();
     }
 }
