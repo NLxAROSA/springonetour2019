e94149ef1807153bf796d9eaad8a55c8f8956970
David Caron <dcaron@xs4all.nl>
2019-03-20 14:24:41 +0100

    Add some fortunes

--- fortune-service/src/main/java/io/pivotal/workshop/workshopfortuneservice/WorkshopFortuneServiceApplication.java
+++ fortune-service/src/main/java/io/pivotal/workshop/workshopfortuneservice/WorkshopFortuneServiceApplication.java
@@ -28,7 +28,18 @@ public class WorkshopFortuneServiceApplication {
 			fortuneRepo.save(new Fortune("There's a 5 in the clock somewhere."));
 			fortuneRepo.save(new Fortune("You will be hungry in an hour."));
 			fortuneRepo.save(new Fortune("Today will be an awesome day!"));
-			log.info("Fortune Repo record count: {}", fortuneRepo.count());
+			fortuneRepo.save(new Fortune("Today you will meet the love of your life"));
+			fortuneRepo.save(new Fortune("Your career will go down the tube!"));
+			fortuneRepo.save(new Fortune("Tomorrow, PVTL will trade at $30!"));
+			fortuneRepo.save(new Fortune("k8s will become the new OpenStack!"));
+			fortuneRepo.save(new Fortune("Next year is the year of Linux on the desktop!"));
+			fortuneRepo.save(new Fortune("Tomorrow free beer!"));
+            fortuneRepo.save(new Fortune("${languageDuJour} will replace Java in a year!"));
+            fortuneRepo.save(new Fortune("Robots will kill everyone!"));
+            fortuneRepo.save(new Fortune("Skynet gains self-awareness tomorrow!"));
+
+
+            log.info("Fortune Repo record count: {}", fortuneRepo.count());
 			fortuneRepo.findAll().forEach(x -> log.debug(x.toString()));
 		};
 
